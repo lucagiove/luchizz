6b984b1676843055408eef99096c1bd7e8d2ef9b
Luca Giovenzana <luca@giovenzana.org>
2016-08-24 11:54:37 +0200

    [REVIEW] restored standard luchizz PS colours
    - restored debian_chroot standard variables
    - restored luchizz prompt
    - kept the EUID var for optimization

--- files/profile/luchizz-profile.sh
+++ files/profile/luchizz-profile.sh
@@ -1,14 +1,13 @@
 
 # START of luchizz bash enhancements
 # Copyright (C) 2014-2015 Luca Giovenzana <luca@giovenzana.org>
-# luchizz-profile.sh (0.0.13)
+# luchizz-profile.sh (0.1.0dev)
 
 # colorized prompt
-
-if [[ ${EUID} == 0 ]] ; then
-    PS1='\[\033[01;31m\]\h\[\033[01;34m\] \W \$\[\033[00m\] '
+if [ $EUID -eq 0 ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]# '
 else
-    PS1='\[\033[01;37m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;37m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 fi
 
 # useful aliases
@@ -76,4 +75,3 @@ if [ -f /usr/local/lib/bash-git-prompt/gitprompt.sh ]; then
     . /usr/local/lib/bash-git-prompt/gitprompt.sh
 fi
 # END of luchizz bash enhancements
-
