9e37542561c91362564115bfd89f7a356270cb08
Luca Giovenzana <luca@giovenzana.org>
2016-08-27 17:18:14 +0200

    [FEATURE] introduced ROADMAP.md

---
+++ ROADMAP.md
@@ -0,0 +1,25 @@
+# Luchizz Roadmap
+
+## future / features
+    TODO setup rkhunter
+    TODO setup sshd security
+    TODO improve a real debug/verbose/normal mode
+    TODO change to argparse
+
+## next version
+
+    TODO print warning on user rename or change also sudoers
+    TODO detect luchizz version for the bash-profile and update
+         probably even better to use an external file to be included and replaced
+         after every luchizz attempt
+    TODO setup mail notification
+    TODO add screenrc profile
+
+## version 0.2
+
+    TODO create setup (completely change structure)
+
+## KNOWN ISSUES
+
+    FIXME handle returncode 1 in case of NO answer to apt-get
+    FIXME handle stdout/err redirection
--- src/luchizz/runner.py
+++ src/luchizz/runner.py
@@ -67,27 +67,6 @@ from luchizz.tasks import (setup_etckeeper,
 # Luchizz script folder
 LUCHIZZ_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# #### future / features
-# TODO setup rkhunter
-# TODO setup sshd security
-# TODO improve a real debug/verbose/normal mode
-# TODO change to argparse
-
-# #### next version
-# TODO print warning on user rename or change also sudoers
-# TODO detect luchizz version for the bash-profile and update
-#      probably even better to use an external file to be included and replaced
-#      after every luchizz attempt
-# TODO setup mail notification
-# TODO add screenrc profile
-
-# #### version 0.2
-# TODO create setup (completely change structure)
-
-# #### KNOWN ISSUES
-# FIXME handle returncode 1 in case of NO answer to apt-get
-# FIXME handle stdout/err redirection
-# FIXME see why doesn't work on raspbmc (bec of motd always printed..)
 
 original_sigint = signal.getsignal(signal.SIGINT)
 
